d68c06ca7f59ae4fae3e6e0f5cb85d4ca4335966
Marton Barcza <marton.barcza@gmail.com>
2018-11-16 15:24:21 +0100

    index page design complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Ingredient.destroy_all
 
+puts "Marton: ingredient database cleared"
 
-
-# !!! should add here that I want to clear the database first
-
-puts "starting seed"
+puts "Marton: starting seed"
 
 Ingredient.create(name: "lemon")
 Ingredient.create(name: "ice")
@@ -21,4 +13,4 @@ Ingredient.create(name: "gin")
 Ingredient.create(name: "tonic")
 Ingredient.create(name: "lime")
 
-puts "seed finished"
+puts "Marton: seed finished"
